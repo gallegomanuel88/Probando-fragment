6f4820af2334681d29286bef984eb19ce34ee5cf
Manuel Alonso Rodrígez <Manuel Alonso Rodrgez>
2016-12-16 16:32:39 +0100

    AtivityMain2 y metodo para llamarla creados

--- app/src/main/java/com/example/galle/fragments/MainActivity.java
+++ app/src/main/java/com/example/galle/fragments/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.galle.fragments;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button botonLlamar;
+    Intent llamarMain2Obj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        llamarMain2Obj = new Intent(MainActivity.this, MainActivity2.class);
+    }
+
+    public void llamarMain2 (View view){
+        startActivity(llamarMain2Obj);
     }
 }
---
+++ app/src/main/java/com/example/galle/fragments/MainActivity2.java
@@ -0,0 +1,13 @@
+package com.example.galle.fragments;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
